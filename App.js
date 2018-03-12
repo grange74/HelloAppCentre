e6c0036e0f979239e491381681fb9dc9b17edecc
Nicolas Grange <nicolas.grange@retrievercommunications.com>
2018-03-12 15:51:00 +1100

    clean up formatting and add demo instructions

--- App.js
+++ App.js
@@ -5,8 +5,12 @@ import { Button, Header } from 'react-native-elements';
 
 const codePushOptions = { checkFrequency: codePush.CheckFrequency.MANUAL };
 
+// change image and colour and the redeploy using 'appcenter codepush release-react'
+
 const image = require('./images/qa.png');
 
+const colour = '#9E1E30';
+        
 class MyApp extends Component {
 
     onButtonPress() {
@@ -17,20 +21,30 @@ class MyApp extends Component {
     }
 
     render() {
-        const rcColour = '#9E1E30';
         return (
                 <View style={{ flex: 1 }}>
                     <Header
-                        centerComponent={{ text: 'Hello App Center', style: { color: '#fff', fontSize: 30 } }}
-                        backgroundColor={rcColour}
+                        centerComponent={{
+                            text: 'Hello App Center',
+                            style: {
+                                color: '#fff',
+                                fontSize: 30 
+                            } 
+                        }}
+                        backgroundColor={colour}
                     />
-                    <View style={{ flex: 1, alignItems: 'center' }}>
+                    <View 
+                        style={{
+                            flex: 1,
+                            alignItems: 'center' 
+                        }}
+                    >
                         <Image source={image} />
                         <Button 
                             large
                             title='Check for updates'
                             onPress={this.onButtonPress}
-                            backgroundColor={rcColour}
+                            backgroundColor={colour}
                         />
                     </View>
                 </View>
